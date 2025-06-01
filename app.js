c888efa242a74cda26e931c895a9635e9512e94b
chen-vv <vchen03@student.ubc.ca>
2025-06-01 10:10:18 -0700

    Add QR code read functionality

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const inputImage = document.getElementById("inputImage");
 const preview = document.getElementById("imagePreview");
 const errorText = document.getElementById("errorText");
+const qrData = document.getElementById("qrData");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const MAX_SIZE = 512;
 
 inputImage.addEventListener(
   "change",
@@ -13,6 +18,13 @@ inputImage.addEventListener(
     }
 
     const fileList = inputImage.files;
+
+    if (fileList.length === 0) {
+      errorText.textContent = "No image selected";
+      qrData.textContent = "";
+      return;
+    }
+
     const file = fileList[0];
 
     if (!file.type.startsWith("image/")) {
@@ -23,13 +35,36 @@ inputImage.addEventListener(
     const img = document.createElement("img");
     img.id = "asdf";
     img.file = file;
+
+    const objectUrl = URL.createObjectURL(img.file);
+    img.src = objectUrl;
     preview.appendChild(img);
 
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      img.src = e.target.result;
+    img.onload = () => {
+      const width = img.naturalWidth;
+      const height = img.naturalHeight;
+
+      const scale = Math.min(MAX_SIZE / width, MAX_SIZE / height, 1);
+      const newWidth = Math.floor(width * scale);
+      const newHeight = Math.floor(height * scale);
+
+      canvas.width = newWidth;
+      canvas.height = newHeight;
+
+      ctx.drawImage(img, 0, 0, newWidth, newHeight);
+
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      const qrCode = jsQR(imageData.data, imageData.width, imageData.height);
+
+      if (qrCode) {
+        qrData.textContent = qrCode.data;
+        console.log("Found QR code", qrCode);
+      } else {
+        qrData.textContent = "No QR code found";
+      }
+
+      URL.revokeObjectURL(objectUrl);
     };
-    reader.readAsDataURL(file);
   },
   false
 );
