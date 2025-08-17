8cbe5ddcf30e893a387faaea9c9275c1db926c93
chen-vv <vchen03@student.ubc.ca>
2025-08-16 20:21:38 -0700

    Chihuahua images show up

--- recaptcha/src/Recaptcha.js
+++ recaptcha/src/Recaptcha.js
@@ -3,16 +3,22 @@ import "./Recaptcha.css";
 
 const Recaptcha = () => {
   const images = Array.from({ length: 9 });
+  const correct_images = [2, 5, 6];
+
   return (
     <div className="Recaptcha">
       <div className="blue-box">
         <p className="captcha-text-smaller">Select all squares with</p>
-        <p className="captcha-text-bold">bicycles</p>
+        <p className="captcha-text-bold">chihuahuas</p>
         <p className="captcha-text-regular">If there are none, click skip</p>
       </div>
       <div className="grid">
         {images.map((_, index) => (
-          <div key={index} className="grid-item"></div>
+          <img
+            src={`/images/chihuahua_muffin/${index + 1}.JPG`}
+            key={index}
+            className="grid-item"
+          ></img>
         ))}
       </div>
       <div>bottom buttons</div>
