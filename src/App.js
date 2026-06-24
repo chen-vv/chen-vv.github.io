1552b41ba11ae8740ce8ea64b3d836f9c03eb138
chen-vv <83690604+chen-vv@users.noreply.github.com>
2026-06-23 23:02:27 -0700

    Add name, GitHub and Linkedin buttons to home screen

--- src/App.js
+++ src/App.js
@@ -2,10 +2,41 @@ import logo from "./logo.svg";
 import "./App.css";
 import Recaptcha from "./Recaptcha";
 
+const openInNewTab = (url) => {
+  window.open(url, '_blank', 'noopener,noreferrer');
+};
+
 function App() {
   return (
     <div className="App">
-      <Recaptcha />
+      <div className="row">
+        <div className="profile-column">
+          <div className="row">
+            <p id="full-name">Vicky Chen</p>
+          </div>
+        </div>
+        <div className="links-column">
+          <div className="row">
+              <button className="rounded-rect-button">
+                  <img src="/icons/document.png" className="rounded-rect-icon" alt="Document"></img>
+                  <h2>Resume</h2>
+              </button>
+          </div>
+          <div className="row">
+              <button className="rounded-rect-button" onClick={() => openInNewTab("https://github.com/chen-vv")}>
+                  <img src="/icons/github.svg" className="rounded-rect-icon" alt="Document"></img>
+                  <h2>GitHub</h2>
+              </button>
+          </div>
+          <div className="row">
+              <button className="rounded-rect-button" onClick={() => openInNewTab("https://www.linkedin.com/in/chen-vv/")}>
+                  <img src="/icons/linkedin.png" className="rounded-rect-icon" alt="Document"></img>
+                  <h2>LinkedIn</h2>
+              </button>
+          </div>
+        </div>
+      </div>
+      {/* <Recaptcha /> */}
     </div>
   );
 }
